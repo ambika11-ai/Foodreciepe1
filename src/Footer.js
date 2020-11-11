d9adde2015626c1e2e8adfb87bdc3c71499d96c0
Ambika s <Ambika s@LAPTOP-RNNGH09U>
2020-11-11 13:52:40 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+function Footer(props)
+{
+  return(
+      <div>
+          
+              <h4> Designed and developed by = ambika </h4>
+
+      </div>
+  )
+}
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+function Header(props)
+{
+  return(
+      <div>
+          <nav>
+              <h1> React fish fry </h1>
+          </nav>
+      </div>
+  )
+}
+export default Header;
